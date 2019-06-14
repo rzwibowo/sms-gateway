0f1a123efb7e5c51afaf1ac9c80bb07623187c03
rzwibowo <rizkiadigunow@gmail.com>
2019-06-14 14:28:05 +0700

    f.4 #7 pindah variabel konfigurasi db ke .env

--- server/api.js
+++ server/api.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const mysql = require('mysql');
+
+require('dotenv').config();
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'sms_gateway'
+    host: process.env.DB_SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 connection.connect(function (err) {
     err ? console.error(err) 
